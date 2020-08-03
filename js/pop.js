dc9aca10d8bb2e8d9e8a778066304fcf34a90d34
Michael Beiene <Michael.Beiene@imail.org>
2020-08-03 02:48:56 -0600

    First commit

---
+++ js/pop.js
@@ -0,0 +1,70 @@
+//(function (d3,topojson) {
+  'use strict';
+
+  const svg = d3.select('svg');
+
+  const projection = d3.geoNaturalEarth1();
+  const pathGen = d3.geoPath().projection(projection);
+
+  svg.append('path')
+      .attr('class', 'sphere');
+    //  .attr('d', pathGen({type: 'Sphere'}));
+
+
+  Promise.all([
+    d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv'),
+    d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+
+  ]).then(([tsvData, topodata]) => {
+  //  console.log(tsvData);
+
+
+  const countryName = tsvData.reduce((accumulator,d) => {
+    accumulator [d.iso_n3] = d.name;
+    return accumulator;
+  },{});
+
+  const continent = tsvData.reduce((accumulator,d) => {
+    accumulator [d.iso_n3] = d.continent;
+    return accumulator;
+  },{});
+
+  const population = tsvData.reduce((accumulator,d) => {
+    accumulator [d.iso_n3] = d.pop_est;
+    return accumulator;
+  },{});
+  /*
+  tsvData.forEach(d => {
+    countryName[d.iso_n3] = d.name;
+
+
+  });
+*/
+    const countries = topojson.feature(topodata, topodata.objects.countries);
+    svg.selectAll('path').data(countries.features)
+      .enter().append('path')
+        .attr('class', 'country')
+        .attr('d', pathGen)
+
+        .append('title')
+
+        .text(d => continent[d.id]+'=>'+'pop:'+population[d.id])
+
+
+
+  //   .text(d => countryName[d.id])
+
+
+
+     ;
+  });
+//  d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv')
+//    .then(data =>);
+//  d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+//    .then(data => {
+
+    //      .append('title')
+        //    .text(d => d.)
+//    });
+
+//}(d3,topojson));
